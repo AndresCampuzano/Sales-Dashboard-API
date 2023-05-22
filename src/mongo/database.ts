9858dc0740f7adbc435dd826e0c85cc79d1dd138
Andres Campuzano Garzon <campuxano88@gmail.com>
2023-05-22 15:20:07 -0500

    build: separates local and prod database

--- src/mongo/database.ts
+++ src/mongo/database.ts
@@ -5,19 +5,20 @@ import { collections } from './collections';
  * Connects to the mongo and sets the collections object
  */
 export async function connectToDatabase() {
-  // Check current environment
-  if (process.env.NODE_ENV !== 'production') {
-    console.log('🚀 Connecting to local database');
-  } else {
-    console.log('🚀 Connecting to Atlas database');
-  }
   const client: mongoDB.MongoClient = new mongoDB.MongoClient(
     process.env.ATLAS_URI as string
   );
-
   await client.connect();
 
-  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+  let db: mongoDB.Db;
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('🚀 Connecting to local database');
+    db = client.db(process.env.DB_NAME);
+  } else {
+    console.log('🚀 Connecting to PRODUCTION database');
+    db = client.db(process.env.DB_NAME);
+  }
 
   collections.clientCollection = db.collection('client_collection');
   collections.itemCollection = db.collection('item_collection');
