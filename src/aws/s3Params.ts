03553ff9dca0258483523517f88a2a063ed9c6f3
Andres Campuzano Garzon <campuxano88@gmail.com>
2023-05-22 19:24:25 -0500

    fix: add image/type to s3 object

--- src/aws/s3Params.ts
+++ src/aws/s3Params.ts
@@ -6,32 +6,24 @@ interface S3Params {
   id: string;
 }
 
-const getContentType = (bf: Buffer) => {
-  const types: { [key: string]: string } = {
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    gif: 'image/gif'
-  };
-  const extension = bf.toString('ascii', 0, 3);
-  return types[extension];
-};
-
 /**
  * Returns the parameters to upload an image to S3
  */
-export const s3Params = (image: string): S3Params => {
-  const buffer = Buffer.from(
-    image.replace(/^data:image\/\w+;base64,/, ''),
+export const s3Params = (base64: string): S3Params => {
+  const base64Data = Buffer.from(
+    base64.replace(/^data:image\/\w+;base64,/, ''),
     'base64'
   );
+  // Getting the file type, ie: jpeg, png or gif
+  const type = base64.split(';')[0].split('/')[1];
   const id = new ObjectId();
   const params = new PutObjectCommand({
     GrantRead: 'uri=http://acs.amazonaws.com/groups/global/AllUsers', // This is necessary to make the image public
     Bucket: process.env.AWS_S3_BUCKET_NAME as string, // bucket that we made earlier
     Key: id.toString(), // Name of the image
-    Body: buffer, // Body which will contain the image in buffer format
+    Body: base64Data, // Body which will contain the image in buffer format
     ContentEncoding: 'base64',
-    ContentType: getContentType(buffer) // Necessary to define the image content-type to view the photo in the browser with the link
+    ContentType: `image/${type}` // Necessary to define the image content-type to view the photo in the browser with the link
   });
   return {
     params,
